120c8a291b76e65f775fb35bb60b8df0b023d23f
András Szabó <szabo.andras.2018@gmail.com>
2023-12-07 18:45:02 +0100

    Add basic error handling (try-catch blocks with user warnings).

--- index.js
+++ index.js
@@ -13,15 +13,27 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  const result = await axios.get(API_URL_END + PAR_LAN_ENG + PAR_SAFE);
-  const jokeText = jokeApiData_htmlText_convert(result.data);
-  res.render("jokes-root.ejs", { joke: jokeText });
+  try {
+    const result = await axios.get(API_URL_END + PAR_LAN_ENG + PAR_SAFE);
+    const jokeText = jokeApiData_htmlText_convert(result.data);
+    res.render("jokes-root.ejs", { joke: jokeText });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).render("jokes-root.ejs", { joke: "Server Error!<br>Sorry about that." });
+  }
+  
 });
 
 app.post("/byword", async (req, res) => {
-  const result = await axios.get(API_URL_END + PAR_LAN_ENG + PAR_WORD + req.body.word);
-  const jokeText = jokeApiData_htmlText_convert(result.data);
-  res.render("jokes-word.ejs", { joke: jokeText, word: req.body.word });
+  try {
+    const result = await axios.get(API_URL_END + PAR_LAN_ENG + PAR_WORD + req.body.word);
+    const jokeText = jokeApiData_htmlText_convert(result.data);
+    res.render("jokes-word.ejs", { joke: jokeText, word: req.body.word });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).render("jokes-word.ejs", { joke: "Server Error!<br>Sorry about that.", word: req.body.word });
+  }
+  
 });
 
 app.listen(port, () => {
@@ -31,17 +43,21 @@ app.listen(port, () => {
 
 
 function jokeApiData_htmlText_convert(jokeData) {
-  switch (jokeData.type) {
-    case "single":
-      return jokeData.joke.split("\n").join("<br>");
-      //break;
-    case "twopart":
-      return jokeData.setup.split("\n").join("<br>") + "<br><br>" + jokeData.delivery.split("\n").join("<br>");
-      //break;
-    default:
-      // Error!
-      console.log("BUG: jokeData.type == '" + jokeData.type + "' not supported!");
-      return "Error: unsupported joke-type found: '" + jokeData.type + "'!<br><br>Please contact the dev.";
-      //break;
+  if (jokeData.error) {
+    return jokeData.message;
+  } else {
+    switch (jokeData.type) {
+      case "single":
+        return jokeData.joke.split("\n").join("<br>");
+        //break;
+      case "twopart":
+        return jokeData.setup.split("\n").join("<br>") + "<br><br>" + jokeData.delivery.split("\n").join("<br>");
+        //break;
+      default:
+        // Error!
+        console.log("BUG: jokeData.type == '" + jokeData.type + "' not supported!");
+        return "Error: unsupported joke-type found: '" + jokeData.type + "'!<br><br>Please contact the dev.";
+        //break;
+    }
   }
 }
